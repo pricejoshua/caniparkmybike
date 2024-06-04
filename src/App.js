3b78fad185a99fc1ac09b5e8b3332644f36a1a8f
Joshua Price <joshuajangprice@gmail.com>
2024-06-04 15:35:26 -0500

    Got things working for the most part

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Autocomplete from './components/Autocomplete';
+import { useState, useEffect } from 'react';
+
+const queryString = (lat, lng) => {
+  return `[out:json];
+  (
+    way["amenity"="bicycle_parking"](around:322,${lat},${lng});
+    relation["amenity"="bicycle_parking"](around:322,${lat},${lng});
+    node["amenity"="bicycle_parking"](around:322,${lat},${lng});
+  );
+  out body;`
+}
+
+async function getRacks(lat, lng) {
+  var result = await fetch('https://overpass-api.de/api/interpreter', {
+    method: 'POST',
+    body: 'data=' + queryString(lat, lng),
+  });
+
+
+  var data = await result.json();
+  console.log(data);
+}
 
 function App() {
+  
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+
+  function setLocation(lat, lng) {
+    setLat(lat);
+    setLng(lng);
+  }
+
+  useEffect(() => {
+    if (lat === 0 && lng === 0) {
+      return;
+    }
+    console.log(lat, lng);
+    setLocation(lat, lng);
+    getRacks(lat, lng);
+  }, [lat, lng]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Autocomplete callbackFn={setLocation} />
       </header>
     </div>
   );
---
+++ src/components/Autocomplete.jsx
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+const Autocomplete = ({ callbackFn }) => {
+  const autocompleteRef = useRef(null);
+  const inputRef = useRef(null);
+  const [address, setAddress] = useState('');
+
+  useEffect(() => {
+    const loadScript = (url, callback) => {
+      let script = document.createElement('script');
+      script.type = 'text/javascript';
+
+      if (script.readyState) {
+        script.onreadystatechange = () => {
+          if (script.readyState === 'loaded' || script.readyState === 'complete') {
+            script.onreadystatechange = null;
+            callback();
+          }
+        };
+      } else {
+        script.onload = () => {
+          callback();
+        };
+      }
+
+      script.src = url;
+      document.getElementsByTagName('head')[0].appendChild(script);
+    };
+
+    // const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    const apiKey = "AIzaSyAQz7PL0lV3TLe_zUFEF29yghBw-MnNfJw";
+
+    loadScript(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`, () => {
+      const autocomplete = new window.google.maps.places.Autocomplete(inputRef.current, {
+        fields: ['geometry']
+      });
+
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        callbackFn(place.geometry.location.lat(), place.geometry.location.lng());
+        setAddress(place.geometry.viewport.toString());
+      });
+
+      autocompleteRef.current = autocomplete;
+    });
+  }, []);
+
+  return (
+    <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Enter an address"
+        style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
+      />
+      <div>
+        <strong>Selected Address:</strong> {address}
+      </div>
+    </div>
+  );
+};
+
+export default Autocomplete;
